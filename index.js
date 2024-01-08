924177fe0e29b9653b09dece94da3fde86f423bd
Kamenchu <iankamenchu@gmail.com>
2024-01-08 05:49:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const main = document.getElementById('main');
+if (main) {
+  main.remove();
+}
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const playerName = "Rebekah"; 
+newHeader.textContent = `${playerName} is the champion`;
+
+document.body.appendChild(newHeader);
